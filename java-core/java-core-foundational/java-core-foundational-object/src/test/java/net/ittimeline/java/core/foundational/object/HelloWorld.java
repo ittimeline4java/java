f3d193936ee9fcf2df4a840fee5efb7a43d23968
ittimeline4java <ittimeline@163.com>
2025-04-01 21:45:24 +0800

    跟光磊学Java从小白到架构师-Java语言-第2章 Java概述与集成开发环境搭建

---
+++ java-core/java-core-foundational/java-core-foundational-object/src/test/java/net/ittimeline/java/core/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.ittimeline.java.core.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author tony 18601767221@163.com
+ * @version 2025/4/1 20:56
+ * @since Java21
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java 21.0.6 & Maven 3.9.9 & Git 2.49.0 & GitHub & IntelliJ IDEA 2024.3.5");
+    }
+}
